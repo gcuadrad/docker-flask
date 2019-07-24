ae0d489f08e58f33b5d051599098fd7d36278d2f
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2019-07-23 23:37:39 -0400

    Add flask examples and html template

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello There!!!!!!!!!!</h1>'
+
+@app.route('/page/<place>')
+def page(place):
+    return '<h1>You are on the '+ place +' page!</h1>'
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return '<h1>You are on the home page!</h1>'
+
+@app.route('/template')
+def template():
+    links = ['https://docker-curriculum.com','https://www.youtube.com/watch?v=Qw9zlE3t8Ko','https://flask.palletsprojects.com']
+    return render_template('example.html', myvar=' Flask example', links=links)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
