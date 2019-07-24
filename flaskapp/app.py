ba020d017281eb628a5065fbb4805adf72c25143
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2019-07-23 22:47:48 -0400

    Update flask to > 1.0

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello There!!@@!!!!!!!!</h1>'
+    return '<h1>Hello There!!!!!!!!!!</h1>'
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
