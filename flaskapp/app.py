8d709ed35a9532295076727952556e97e81db575
Gonzalo Cuadra <gonzalo.cuadra@frogmi.com>
2019-07-24 16:26:03 -0400

    Add guestbook example

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello There!!!!!!!!!!</h1>'
+    return render_template('index.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+
+    return render_template('index.html', name=name, comment=comment)
 
 @app.route('/page/<place>')
 def page(place):
