2267ea1bb90ce0470d2db1c23266b8f330e2016e
Gonzalo Cuadra <gonzalo.cuadra@frogmi.com>
2019-07-25 14:32:45 -0400

    Connect mysql, add comment model

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
-import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:example@172.17.0.1:3306/mydb'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:example@172.17.0.1:3306/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-db = flask_sqlalchemy.SQLAlchemy(app)
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    comment = db.Column(db.String(1000))
 
 @app.route('/')
 def index():
